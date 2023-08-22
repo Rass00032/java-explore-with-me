e44bfd60b9529f112814a5920f216d399a6d490f
Ravil <Rass00032@yandex.ru>
2023-08-22 18:10:18 +0400

    Логи

--- stats-service/src/main/java/ru/practicum/service/StatsServiceImpl.java
+++ stats-service/src/main/java/ru/practicum/service/StatsServiceImpl.java
@@ -1,6 +1,7 @@
 package ru.practicum.service;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import ru.practicum.dto.RequestDto;
@@ -8,24 +9,27 @@ import ru.practicum.dto.StatsDTO;
 import ru.practicum.model.RequestModel;
 import ru.practicum.repository.StatsRepository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
-
+@Slf4j
 @Service
 @AllArgsConstructor
 public class StatsServiceImpl {
     private final StatsRepository statsRepository;
     private final ModelMapper mapper;
 
-
+    @Transactional
     public RequestModel save(RequestDto requestDto) {
         RequestModel requestModel = mapper.map(requestDto, RequestModel.class);
         statsRepository.save(requestModel);
+        log.info("Запрос {} добавлен дата: {}", requestModel.getUri());
         return requestModel;
     }
 
-    public List<StatsDTO> get(String start, String end, List<String> uri, boolean isUnique) {
 
+    public List<StatsDTO> get(String start, String end, List<String> uri, boolean isUnique) {
+        log.info("Получение запросов");
         if (isUnique) {
             return statsRepository.statisticsUniqueIp(start, end, uri);
         } else {
