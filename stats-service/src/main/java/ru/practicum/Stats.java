9e8d358b62138a0bd2bf3bd4b991438e90e4125c
Ravil <Rass00032@yandex.ru>
2023-08-23 11:05:01 +0400

    Логи

--- stats-service/src/main/java/ru/practicum/Stats.java
+++ stats-service/src/main/java/ru/practicum/Stats.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Stats
-{
-    public static void main( String[] args )
-    {
+public class Stats {
+    public static void main(String[] args) {
         SpringApplication.run(Stats.class, args);
     }
 }
